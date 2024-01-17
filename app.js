7b1b673a52bc4e231ac3e1036147f0700d541886
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-01-17 19:56:07 -0300

    .

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-alert("Boas vinda ao jogo do número secreto");
+    alert("Boas vinda ao jogo do número secreto");
 
-let numeroSecreto = 29;
+    //Gerando número aleatório e convertenho string pra numérico
+    let numeroSecreto = parseInt(Math.random() * 10 + 1);
+    
+    let numeroDigitado;
 
-let numeroDigitado;
+    let tentativas = 1;
 
-let tentativas = 1;
+    while (numeroDigitado != numeroSecreto)
+    {
+    numeroDigitado = prompt("Escolha um número entre 1 e 30: ");
 
-while (numeroDigitado != numeroSecreto)
-{
-numeroDigitado = prompt("Escolha um número entre 1 e 30: ");
+    if(numeroDigitado == numeroSecreto){
+        break;
+    }
 
-if(numeroDigitado == numeroSecreto){
-    alert(`Você acertou, o número secreto é ${numeroSecreto}, voçê acertou após ${tentativas} tentativas, parabéns!`);
-}
+    else if(numeroDigitado < numeroSecreto)
+    {
+    alert(`O número secreto é maior que ${numeroDigitado}`);
+    }else
+    {
+    alert(`O número secreto é menor que ${numeroDigitado}`);
+    }
 
-else if(numeroDigitado < numeroSecreto)
-{
-alert(`O número secreto é maior que ${numeroDigitado}`);
-}else
-{
-alert(`O número secreto é menor que ${numeroDigitado}`);
-}
+    tentativas++;
 
-tentativas++;
+    }
 
-}
-        
+    //Operador Ternário
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
+    alert(`Você acertou, o número secreto é ${numeroSecreto}, voçê acertou após ${tentativas} ${palavraTentativa}, parabéns!`);
+
+
+            
 
 
 
