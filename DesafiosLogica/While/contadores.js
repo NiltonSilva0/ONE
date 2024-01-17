fc6e17832c630867dc5bdc73479271a3ecb8beaa
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-01-17 17:53:09 -0300

    While

---
+++ DesafiosLogica/While/contadores.js
@@ -0,0 +1,40 @@
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// let contador = 1;
+
+// while (contador <= 10)
+// {
+//     console.log(`Número ${contador}`);
+//     contador++;
+// }
+
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// let contador = 10;
+
+//  while (contador >= 0)
+//  {
+//     console.log(`Número ${contador}`);
+//     contador--;
+// }
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+// let contador = prompt("Informe um número para contagem regressiva");
+
+
+// while (contador >= 0) 
+// {
+// console.log(`Número ${contador}`);
+// contador--;
+// }
+
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+let numero = prompt("Informe um número para contagem progressiva");
+let contador = 0;
+
+
+while (contador <= numero)  
+{
+console.log(`Número ${contador}`);
+contador++;
+}
