f8056c2e51bc12395380e0c914eae04eb3f48fc8
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-01-17 16:06:37 -0300

    número secreto

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ let numeroSecreto = 29;
 
 let numeroUser = prompt("Escolha um número entre 1 e 30: ");
 
-if (numeroUser == numeroSecreto) 
+
+
+
+while (numeroUser != numeroSecreto)
 {
-    alert(`Você acertou, o número secreto é ${numeroSecreto}, parabéns!`);      
-}else 
-{
-    while (numeroUser != numeroSecreto)
+    alert("Voçê errou! Tente novamente!");
     numeroUser = prompt("Escolha um número entre 1 e 30: ");
+
+    if(numeroUser < numeroSecreto)
+{
+    alert(`O número secreto é maior que ${numeroUser}`);
+}else if (numeroUser > numeroSecreto)
+{
+    alert(`O número secreto é menor que ${numeroUser}`);
 }
 
+}
+    alert(`Você acertou, o número secreto é ${numeroSecreto}, parabéns!`);      
+
+
+
+
+
+    
 
