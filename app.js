6064b1a979ef2b0d9d3ce58bc5b648a2f43e7f73
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-01-17 17:14:11 -0300

    .

--- app.js
+++ app.js
@@ -2,29 +2,34 @@ alert("Boas vinda ao jogo do número secreto");
 
 let numeroSecreto = 29;
 
-let numeroUser;
+let numeroDigitado;
 
+let tentativas = 1;
 
-
-
-while (numeroUser != numeroSecreto)
+while (numeroDigitado != numeroSecreto)
 {
-    numeroUser = prompt("Escolha um número entre 1 e 30: ");
+numeroDigitado = prompt("Escolha um número entre 1 e 30: ");
 
-    if(numeroUser < numeroSecreto)
-{
-    alert(`O número secreto é maior que ${numeroUser}`);
-}else if (numeroUser > numeroSecreto)
-{
-    alert(`O número secreto é menor que ${numeroUser}`);
+if(numeroDigitado == numeroSecreto){
+    alert(`Você acertou, o número secreto é ${numeroSecreto}, voçê acertou após ${tentativas} tentativas, parabéns!`);
 }
 
+else if(numeroDigitado < numeroSecreto)
+{
+alert(`O número secreto é maior que ${numeroDigitado}`);
+}else
+{
+alert(`O número secreto é menor que ${numeroDigitado}`);
 }
-    alert(`Você acertou, o número secreto é ${numeroSecreto}, parabéns!`);      
+
+tentativas++;
+
+}
+        
 
 
 
 
 
-    
+
 
