f0be9b8e71ceb0e941115aafc89636a9891f570c
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-01-18 01:10:21 -0300

    .

--- DesafiosLogica/Funções e Listas/app.js
+++ DesafiosLogica/Funções e Listas/app.js
@@ -50,3 +50,106 @@ function soma()
 let botaoSoma = document.getElementById("soma");
 
 botaoSoma.addEventListener("click", soma);
+
+//Criar uma função que exibe "Olá, mundo!" no console.
+
+function mensagem(){
+    console.log("Olá, mundo!");
+}
+
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+function mensagemNome(nome){
+    console.log(`Olá, ${nome}!`);
+}
+
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+function dobro(numero){
+    return numero * 2;
+}
+
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+function media(numero1, numero2, numero3){
+    return (numero1 + numero2 + numero3) / 3;
+}
+
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+function maior(numero1, numero2){
+    return numero1 > numero2? numero1 : numero2;
+}
+
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo.
+
+function multiplicacao(numero){
+    return numero * numero;
+}
+
+//Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+
+function imc(altura, peso){
+    return peso / (altura * altura);
+}
+
+//Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
+function fatorial(numero){
+    let fat = 1;
+    for(let i = numero; i > 1; i--){
+        fat *= i;
+    }
+    return fat;
+}
+
+//Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+function dolar(valor){
+    return valor * 4.80;
+}
+
+//Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+
+function areaPerimetroRetangular(altura, largura){
+    let area = altura * largura;
+    let perimetro = 2 * (altura + largura);
+    return `A área é ${area} e o perímetro é ${perimetro}`;
+}
+
+//Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+
+function areaPerimetroCircular(raio){
+    let area = Math.PI * (raio * raio);
+    let perimetro = 2 * Math.PI * raio;
+    return `A área é ${area} e o perímetro é ${perimetro}`;
+}
+
+//Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+
+function tabuada(numero){
+    for(let i = 1; i <= 10; i++){
+        console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do número secreto";
+        // let titulo = document.querySelector("h1");
+        // titulo.innerHTML = "Jogo do número secreto";
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+        // let paragrafo = document.querySelector("p");
+        // paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 
-function verificarChute()
-{
-    let chute = document.querySelector("container__input");
-    chute.innerHTML = 
-}
+        let numeroSecreto = numeroAleatorio();
+        let tentativas = 1;
+        exibirMensagemInicial();
+        function exibirTextoNaTela(tag, texto){
+            let campo = document.querySelector(tag);
+            campo.innerHTML = texto;
+        }
+        function numeroAleatorio(){
+            return parseInt(Math.random() * 10 + 1);
+        }
+        function verificarChute(){
+            let chute = document.querySelector("input").value;
+
+            if (chute == numeroSecreto){
+                exibirTextoNaTela("h1", "Acertou!");
+                let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+                let mensagemTentativa = `Voçê descobriu o número secreto depois de ${tentativas} ${palavraTentativa}`
+                exibirTextoNaTela("p", mensagemTentativa);
+                document.getElementById("reiniciar").removeAttribute("disabled");
+            }else if (chute > numeroSecreto){
+                exibirTextoNaTela("p", "O numero secreto é menor");
+            }else{
+                exibirTextoNaTela("p", "O numero secreto é maior");
+            }
+            tentativas++;
+            limpaCampos();
+        }
+
+        function limpaCampos(){
+            chute = document.querySelector("input");
+            chute.value = "";
+        }
+
+        function exibirMensagemInicial(){
+            exibirTextoNaTela("h1", "Jogo do número secreto");
+        exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+        }
+
+        function reiniciarJogo(){
+        numeroSecreto = numeroAleatorio();
+        limpaCampos();
+        tentativas = 1;
+        exibirMensagemInicial();
+        document.getElementById("reiniciar").setAttribute("disabled", true);
+        }
+
+        
+
+
+        
+        
+
+
